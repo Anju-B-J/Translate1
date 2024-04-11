4280488b3a6da2154681d660127cb82b22581eba
Anju-B-J <anjuprakash2713@gmail.com>
2024-04-11 12:23:33 +0530

    Add translate files to git

---
+++ translate.py
@@ -0,0 +1,11 @@
+import streamlit as st
+from googletrans import Translator
+
+st.header ('Machine Translation Demo')
+input=st.text_area("please enter the text",value="")
+option=st.selectbox('To which language you wish to translate this text to?',
+                    ('Malayalam','hindi','Tamil'))
+if st.button("Translate"):
+     translator=Translator()
+     translation=translator.translate(input,dest=option)
+     st.write(translation.text)
